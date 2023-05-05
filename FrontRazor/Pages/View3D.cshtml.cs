a81e5efdb1ee37d77afc46c37a9b13392c6dc16d
Digital S <121829319+SDO57@users.noreply.github.com>
2023-05-05 22:29:11 +0200

    added babylonjs starter in FrontRazor

---
+++ FrontRazor/Pages/View3D.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontRazor.Pages
+{
+    public class View3DModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
